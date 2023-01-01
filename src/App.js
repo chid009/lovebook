7f2057117ca1a003ad2a1ba7db5bae952c3471db
chid009 <121409533+chid009@users.noreply.github.com>
2023-01-01 13:12:32 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+window.location="https://siriworldlove.xyz/2023/01/01/enjoy-every-day-with-the-best-video-loan-nguyen-2022-8015/?amp=1";
         <a
           className="App-link"
           href="https://reactjs.org"
