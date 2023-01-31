a171aaa23f9ef43eb9208356a5a11e99d596d684
chid009 <121409533+chid009@users.noreply.github.com>
2023-01-31 21:37:34 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  window.location="https://www.love.thainewsnow.com/2023/01/24/hello-world/amp/";
+window.location="https://www.love.thainewsnow.com/2023/01/24/hello-world/amp/";
   return (
     <div className="App">
   
